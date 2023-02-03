0e45fab613dcfdaf0ebfe5809cf55820d9481f96
1955ii <quintin.luong@bp.com>
2023-02-03 17:55:06 +0000

    uncommented example

--- test/app1/src/App.tsx
+++ test/app1/src/App.tsx
@@ -28,61 +28,61 @@ function App() {
         <RemoteButton/>
       </React.Suspense>
       <React.Suspense fallback="loading">
-        {/* <InnerApp /> */}
+        <InnerApp />
       </React.Suspense>
     </>
   );
 }
+
+const remoteModule1 = {
+  url: "http://localhost:3002/remoteEntry.js",
+  scope: "app2",
+  module: "./App",
+};
+
+const remoteModule2 = {
+  url: "http://localhost:3002/remoteEntry.js",
+  scope: "app2",
+  module: "./Button",
+};
+
+getModule({
+  url: "http://localhost:3002/remoteEntry.js",
+  scope: "app2",
+  module: "./App",
+}).then(({ routes }: any) => {
+  //now I have access to the export named 'routes'
+});
+
+const InnerApp = memo(() => {
+  // const t = useRemoteModule(remoteModule1);
+  // const a = useRemoteModule(remoteModule2);
+  // const b = useRemoteModule(remoteModule2);
+  // const c = useRemoteModule(remoteModule2);
+  console.log("render");
+
+  return <></>;
+});
+
 export default App;
-// const remoteModule1 = {
-//   url: "http://localhost:3002/remoteEntry.js",
-//   scope: "app2",
-//   module: "./App",
-// };
 
-// const remoteModule2 = {
-//   url: "http://localhost:3002/remoteEntry.js",
-//   scope: "app2",
-//   module: "./Button",
-// };
+class ErrorBoundary extends React.Component {
+  constructor(props: any) {
+    super(props);
+    this.state = { hasError: false };
+  }
 
-// getModule({
-//   url: "http://localhost:3002/remoteEntry.js",
-//   scope: "app2",
-//   module: "./App",
-// }).then(({ routes }: any) => {
-//   //now I have access to the export named 'routes'
-// });
+  static getDerivedStateFromError(error: any) {
+    return { hasError: true };
+  }
 
-// const InnerApp = memo(() => {
-//   // const t = useRemoteModule(remoteModule1);
-//   // const a = useRemoteModule(remoteModule2);
-//   // const b = useRemoteModule(remoteModule2);
-//   // const c = useRemoteModule(remoteModule2);
-//   console.log("render");
+  render() {
+    //@ts-expect-error
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong.</h1>;
+    }
 
-//   return <></>;
-// });
-
-// export default App;
-
-// class ErrorBoundary extends React.Component {
-//   constructor(props: any) {
-//     super(props);
-//     this.state = { hasError: false };
-//   }
-
-//   static getDerivedStateFromError(error: any) {
-//     return { hasError: true };
-//   }
-
-//   render() {
-//     //@ts-expect-error
-//     if (this.state.hasError) {
-//       // You can render any custom fallback UI
-//       return <h1>Something went wrong.</h1>;
-//     }
-
-//     return this.props.children;
-//   }
-// }
+    return this.props.children;
+  }
+}
